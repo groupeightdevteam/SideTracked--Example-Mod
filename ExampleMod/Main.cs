c7b79a84a00c6a6874be22ed9221a3ddf78bddcf
Lukas Olson <lukasdragon@live.ca>
2021-02-02 10:36:11 -0800

    Added files

---
+++ ExampleMod/Main.cs
@@ -0,0 +1,19 @@
+﻿using Game.Modding.Attributes;
+using Game.Modding.Interfaces;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExampleMod
+{
+    [Mod("ExampleMod", "1.0")]
+    public class Main : IMod
+    {
+        public void Initialize()
+        {
+            Debug.Log("ExampleMod is being initialized...");
+
+            var harmony = new Harmony("com.greyblockgames.olson.ExampleMod");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ ExampleMod/Patches/GravityAbilityPatch.cs
@@ -0,0 +1,18 @@
+﻿using Game.Character.Abilities;
+using Game.Character.Core;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExampleMod
+{
+    [HarmonyPatch(typeof(GravityAbility))]
+    [HarmonyPatch("FixedProcessAbility")]
+    public class GravityAbilityPatch
+    {
+        static bool Prefix(GravityAbility __instance, ref float ___gravityMultiplier, ref CharacterHandler ___Handler)
+        {
+            ___Handler.AddMovement(Physics.gravity * (-___gravityMultiplier * Time.fixedDeltaTime));
+            return false;
+        }
+    }
+}
